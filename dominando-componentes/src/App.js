7c2fcb6aa50977001362cfbfa99596f9228d592d
Emerson Santana <emerson.santana@gitscrum.com>
2020-04-12 00:41:56 -0300

    Initial commit

---
+++ dominando-componentes/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import MeuComponente from './components/MeuComponente';
+
+function App() {
+  return <MeuComponente />
+}
+
+export default App;
